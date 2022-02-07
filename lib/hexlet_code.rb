f651d40ed579c6dc1128ba494f0e39daf046de55
Diopus <97055894+Diopus@users.noreply.github.com>
2022-02-07 22:52:26 +0100

    Fix autoload #2

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -2,11 +2,12 @@
 
 require "active_support/inflector"
 require_relative "hexlet_code/form_for"
+require_relative "hexlet_code/tag"
 require_relative "hexlet_code/version"
 # comment
 module HexletCode
-  project_path = File.expand_path(".")
-  autoload(:Tag, [project_path, "/lib/hexlet_code/tag.rb"].join)
+  # project_path = File.expand_path(".")
+  # autoload(:Tag, [project_path, "/lib/hexlet_code/tag.rb"].join)
 
   class Error < StandardError; end
   # Your code goes here...
