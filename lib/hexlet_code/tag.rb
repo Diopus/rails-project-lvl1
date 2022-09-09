16ec058944b5a9f5a7f55237fa14af3b6804545c
Diopus <97055894+Diopus@users.noreply.github.com>
2022-09-09 17:32:29 +0200

    Added single tags filtering

--- lib/hexlet_code/tag.rb
+++ lib/hexlet_code/tag.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 module HexletCode
+  SINGLE_TAGS = %w[br img input].freeze
+
   class Tag
     def self.build(name, options = {}, &_block)
       fields = options.map { |k, v| " #{k}=\"#{v}\"" }.join
-      return "<#{name}#{fields}>" if name == 'input'
+      return "<#{name}#{fields}>" if SINGLE_TAGS.include?(name)
 
       content = block_given? ? yield : ''
       "<#{name}#{fields}>#{content}</#{name}>"
